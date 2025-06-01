b760fa4e88900bd38af780e2d76fe4057e2dcf46
emir91 <vranacemir@gmail.com>
2025-06-01 20:55:25 +0200

    remove comment

--- src/components/LatestAds/LatestAdCard.tsx
+++ src/components/LatestAds/LatestAdCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './LatestAds.module.css'; // Adjust this path as needed
+import styles from './LatestAds.module.css';
 
 interface AdSummary {
   makeYear: string;
